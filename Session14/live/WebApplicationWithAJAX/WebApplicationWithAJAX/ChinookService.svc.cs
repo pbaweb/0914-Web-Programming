6b14e1a622d3e81f558bb518fcce574b2b607622
Christian Wahl <chrwahl@gmail.com>
2014-12-09 13:10:43 +0100

    added AJAX example

---
+++ Session14/live/WebApplicationWithAJAX/WebApplicationWithAJAX/ChinookService.svc.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+//add a reference to the lib.
+using System.ServiceModel.Web;
+
+namespace WebApplicationWithAJAX
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ChinookService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select ChinookService.svc or ChinookService.svc.cs at the Solution Explorer and start debugging.
+    public class ChinookService : IChinookService
+    {
+
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json, UriTemplate = "artists")]
+        public IEnumerable<Artist> getAllArtists()
+        {
+            DataChinookDataContext cdc = new DataChinookDataContext("Data Source=NO142145;Initial Catalog=Chinook;Integrated Security=True");
+            cdc.DeferredLoadingEnabled = false;
+
+            var artists = from a in cdc.Artists select a;
+            return artists;
+        }
+
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json, UriTemplate = "artists/{id}")]
+        public Artist getArtistsById(string id)
+        {
+            DataChinookDataContext cdc = new DataChinookDataContext("Data Source=NO142145;Initial Catalog=Chinook;Integrated Security=True");
+            cdc.DeferredLoadingEnabled = false;
+
+            var artist = cdc.Artists.First(a => a.ArtistId == Int32.Parse(id));
+            return artist;
+        }
+    }
+}
---
+++ Session14/live/WebApplicationWithAJAX/WebApplicationWithAJAX/Default.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplicationWithAJAX.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <script>
+        var readystatehandler = function (e) {
+            if (e.target.readyState == 4) {
+                var obj = JSON.parse(e.target.response);
+                document.getElementById('list').innerHTML = '';
+                for (var i = 0; i < obj.length; i++) {
+                    document.getElementById('list').innerHTML += '<li>' + obj[i].Name + '</li>';
+                }
+            }
+        };
+
+        window.addEventListener('load', function () {
+            var client = new XMLHttpRequest();
+            client.addEventListener('readystatechange', readystatehandler, false);
+            client.open('GET', '/ChinookService.svc/chinook/artists');
+            client.send();
+        }, false);
+    </script>
+</head>
+<body>
+    
+    <ul id="list"></ul>
+</body>
+</html>
